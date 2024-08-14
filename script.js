c79492c9891f755b9a43d113ec489fbe947ac472
AScodecraft <152480714+AScodecraft@users.noreply.github.com>
2024-08-14 02:26:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+  });
+  
